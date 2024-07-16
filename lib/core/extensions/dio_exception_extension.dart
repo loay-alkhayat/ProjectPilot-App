0499d371ef00db9fc92663c63269302e874d504e
Loay Sy <lloayalkhayat20011@gmail.com>
2024-07-16 15:13:44 +0300

    first commit

--- lib/core/extensions/dio_exception_extension.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:dio/dio.dart';
-//
-//
-// extension _DioExceptionN on DioException {
-//   DioException noN() => DioException(requestOptions: null,response: ,message: ,error: null,type: DioExceptionType.)
-// }
